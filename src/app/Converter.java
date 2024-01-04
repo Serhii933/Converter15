9c84043394d39fb93412d0ec8a68e742e56aff06
Serhii933 <Subota933@gmail.com>
2024-01-04 16:08:48 +0200

    add kilometres for miles

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -4,6 +4,9 @@ public class Converter {
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
+    }
 
     public static void main(String[] args) {
         double miles = 10;
